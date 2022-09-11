d515b75ffdd1426fe9b0e1a4d3479387ed544a70
Divyasingh001 <102237112+Divyasingh001@users.noreply.github.com>
2022-09-11 09:13:28 -0700

    Add files via upload

---
+++ absalutevalue.java
@@ -0,0 +1,10 @@
+package java1;
+import java.util.*;
+import java.lang.Math;
+public class absalutevalue{
+	public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	System.out.println(Math.abs(n));
+	}
+}
